5f332c34a3e46174081d56fa056fd9433d2112e2
Gabriel Sobral <103462795+GabrielSobral8@users.noreply.github.com>
2022-08-22 16:27:43 -0300

    Update credit.py

--- credit.py
+++ credit.py
@@ -1,41 +1,37 @@
-def main():
-  nmrCartao = input("Insira o número do cartão de crédito: ")
-#funcao para informar se é valido ou nao o numero do cartao após passar pelo algoritimo de luhn
-  if validarCartao(nmrCartao):
-    bandeira = getBandeira(nmrCartao)
-    print(bandeira)
-  else:
-    print("INVÁLIDO")
 #algoritimo de luhn para definir a validade do cartao, e se valido, qual a bandeira do mesmo
 def validarCartao(cartao):
-  total = 0
+  soma = 0
 
   for i in range(len(cartao) - 2, -1, -2):
-    vezesDois = int(cartao[i]) * 2
-    if vezesDois > 9:
-      total += vezesDois - 9
+    if int(cartao[i]) * 2 > 9:
       #qualquer numero maior que 9 até 19 subtraido de 9, é o valor da soma dos proprios algarismos
+      soma += (int(cartao[i]) * 2) - 9
     else:
-      total += vezesDois
+      soma += int(cartao[i]) * 2
 
   for i in range(len(cartao) - 1, -1, -2):
-    total += int(cartao[i])
+    soma += int(cartao[i])
 
-  if total % 10 == 0:
+  if soma % 10 == 0:
     return True
-  else
-  return False
+  else:
+    return False
+
 #após passar pela verificacao, aqui  é definido qual a bandeira do cartao seguindo as informacoes passadas no topico 2 do pset
-def getBandeira(cartao):
-  if (len(cartao) == 13 or len(cartao) == 16) and cartao[0] == '4':
-    return 'VISA'
+def mostraBandeira(cartao):
+  if cartao[0] == '4' and len(cartao) in [13, 16]:
+        print('VISA')
+  else:
+      if cartao[0:2] in ['34', '37'] and len(cartao) == 15:
+          print('AMEX')
+      elif cartao[0:2] in ['51', '52', '53', '54', '55'] and len(cartao) == 16:
+          print('MASTERCARD')
+      else:
+          print('INVÁLIDO')
 
-  if len(cartao) == 16 and cartao[0:2] in ['51', '52', '53', '54', '55']:
-      return 'MASTERCARD'
-
-  if len(cartao) == 15 and cartao[0:2] in ['34', '37']:
-      return 'AMEX'
-
-  return 'OUTRA BANDEIRA'
-
-main()
+nmrCartao = input("Insira o número do cartão de crédito: ")
+#funcao para informar se é valido ou nao o numero do cartao após passar pelo algoritimo de luhn
+if validarCartao(nmrCartao):
+  mostraBandeira(nmrCartao)
+else:
+  print("INVÁLIDO")
